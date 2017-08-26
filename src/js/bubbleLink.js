264978ad39e82097d026f45b20e5c7a964e1c179
CornyElephant <eldraco@mac.com>
2017-08-26 06:40:18 -0400

    Remove unnecessary Component import from bubbleLink.js

--- src/js/bubbleLink.js
+++ src/js/bubbleLink.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 
 function BubbleLink(props) {
 	return (
@@ -8,5 +8,4 @@ function BubbleLink(props) {
 		</a>
 	);
 }
-
 export default BubbleLink;
